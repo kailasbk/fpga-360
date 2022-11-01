750c9f9a157dec9d160266bdb07406230500b4ba
Kailas B. Kahler <kailasbk230@gmail.com>
2022-11-01 17:52:27 -0400

    Revise fp32 multiplier

--- src/fp/fp32_mul.sv
+++ src/fp/fp32_mul.sv
@@ -30,43 +30,46 @@ module fp32_mul (
   logic [8:0] a_exp, b_exp;
   logic [23:0] a_frac, b_frac;
   always_ff @(posedge clk_in) begin
-    // a and b signs
-    a_sign <= a_in[31];
-    b_sign <= b_in[31];
+    if (valid_in) begin
+      // a and b signs
+      a_sign <= a_in[31];
+      b_sign <= b_in[31];
 
-    // a and b biased exponents
-    a_exp <= {1'b0, a_in[30:23]};
-    b_exp <= {1'b0, b_in[30:23]};
+      // a and b biased exponents
+      a_exp <= {1'b0, a_in[30:23]};
+      b_exp <= {1'b0, b_in[30:23]};
 
-    // a and b fractions
-    a_frac <= {a_lead, a_in[22:0]};
-    b_frac <= {b_lead, b_in[22:0]};
+      // a and b fractions
+      a_frac <= {a_lead, a_in[22:0]};
+      b_frac <= {b_lead, b_in[22:0]};
+    end
   end
 
   logic is_zero, is_infinity;
+  pipe #(
+    .LATENCY(4),
+    .WIDTH(2)
+  ) flag_pipe (
+    .clk_in,
+    .data_in({(a_exp == 9'h00) || (b_exp == 9'h00), (a_exp == 9'hFF) || (b_exp == 9'hFF)}),
+    .data_out({is_zero, is_infinity})
+  );
+
   logic [8:0] exp_sum;
   logic [8:0] c_base_exp;
-  always_ff @(posedge clk_in) begin
-    is_zero <= (a_exp == 9'b0) || (b_exp == 9'b0);
-    is_infinity <= (a_exp == 9'd31) || (b_exp == 9'd31);
-    exp_sum <= a_exp + b_exp;
-
-    // calculate pre-normalization c exponent
-    c_base_exp <= exp_sum - 9'd127;
-  end
-
   logic [47:0] frac_prod;
   logic [47:0] c_base_frac;
   always_ff @(posedge clk_in) begin
-    // calculate pre-normalization c fraction
+    exp_sum <= a_exp + b_exp;
     frac_prod <= a_frac * b_frac;
+
+    c_base_exp <= exp_sum - 9'd127;
     c_base_frac <= frac_prod;
   end
 
   logic [8:0] c_normalized_exp;
   logic [47:0] c_normalized_frac;
   always_ff @(posedge clk_in) begin
-    // calculate normalized c exponent and fraction
     if (c_base_frac[47]) begin
       // frac = 1x.xxxx
       c_normalized_exp <= c_base_exp + 9'd1;
@@ -76,6 +79,9 @@ module fp32_mul (
       c_normalized_exp <= c_base_exp;
       c_normalized_frac <= {c_base_frac[46:0], 1'b0};
     end
+
+    // note: this does not handle 00.xxxx according to IEEE-754 floats
+    // but it says extra shifting stages that would be almost never used
   end
 
   logic [8:0] c_rounded_exp;
@@ -88,24 +94,14 @@ module fp32_mul (
     .rounded_frac_out(c_rounded_frac)
   );
 
-  logic is_zero_out, is_infinity_out;
-  pipe #(
-    .LATENCY(3),
-    .WIDTH(2)
-  ) flag_pipe (
-    .clk_in,
-    .data_in({is_zero, is_infinity}),
-    .data_out({is_zero_out, is_infinity_out})
-  );
-
   logic [7:0] c_exp;
   logic [22:0] c_frac;
   always_ff @(posedge clk_in) begin
-    if (is_zero_out || (c_rounded_exp >= 9'd384 && !is_infinity_out)) begin
+    if (is_zero || (c_rounded_exp >= 9'd384 && !is_infinity)) begin
       // round to zero
       c_exp <= 8'b0000_0000;
       c_frac <= c_rounded_frac;
-    end else if (is_infinity_out || c_rounded_exp >= 9'd256) begin
+    end else if (is_infinity || c_rounded_exp >= 9'd256) begin
       // round to infinity
       c_exp <= 8'b1111_1111;
       c_frac <= c_rounded_frac;
