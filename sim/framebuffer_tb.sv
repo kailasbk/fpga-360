99f2e140a78dd9be65d4ed99f629f192e5c3a38a
Kailas B. Kahler <kailasbk230@gmail.com>
2022-11-11 14:00:56 -0500

    Implement preliminary framebuffer

---
+++ sim/framebuffer_tb.sv
@@ -0,0 +1,101 @@
+`default_nettype none
+`timescale 1ns / 1ps
+
+`include "sim/testing.svh"
+`include "sim/ip/blk_mem_gen_v8_4.v"
+`include "sim/ip/frame_ram.v"
+`include "sim/ip/depth_ram.v"
+
+`include "src/util/pipe.sv"
+`include "src/util/ram.sv"
+`include "src/util/valid_pipe.sv"
+`include "src/graphics/vga.sv"
+`include "src/graphics/framebuffer.sv"
+
+module framebuffer_tb;
+
+  logic gpu_clk_in;
+  logic vga_clk_in;
+  logic rst_in;
+
+  logic clear_in;
+  logic switch_in;
+  logic valid_in;
+  logic [8:0] x_in;
+  logic [7:0] y_in;
+  logic [7:0] z_in;
+  logic [11:0] rgb_in;
+
+  logic hsync_out;
+  logic vsync_out;
+  logic [11:0] rgb_out;
+
+  framebuffer uut (
+    .gpu_clk_in,
+    .vga_clk_in,
+    .rst_in,
+    .clear_in,
+    .switch_in,
+    .valid_in,
+    .x_in,
+    .y_in,
+    .z_in,
+    .rgb_in,
+    .hsync_out,
+    .vsync_out,
+    .rgb_out
+  );
+
+  always begin
+    #5;
+    gpu_clk_in = !gpu_clk_in;
+  end
+
+  always begin
+    #20;
+    vga_clk_in = !vga_clk_in;
+  end
+
+  initial begin
+    $dumpfile("waveform.vcd");
+    $dumpvars(3, framebuffer_tb);
+    gpu_clk_in = 0;
+    vga_clk_in = 0;
+    rst_in = 0;
+    #10;
+    rst_in = 1;
+    #50;
+    rst_in = 0;
+    valid_in = 0;
+    clear_in = 0;
+    switch_in = 0;
+    #10;
+
+    #1000000;
+
+    for (int j = 0; j < 240; j = j + 1) begin
+      for (int i = 0; i < 320; i = i + 1) begin
+        valid_in = 1;
+        x_in = i;
+        y_in = j;
+        z_in = 8'b0;
+        rgb_in = (j * 320) + i;
+        #10;
+      end
+    end
+    valid_in = 0;
+    #10;
+
+    switch_in = 1;
+    #10;
+    switch_in = 0;
+    #10;
+
+    #30000000;
+
+    $finish;
+  end
+
+endmodule;
+
+`default_nettype wire
--- src/graphics/framebuffer.sv
+++ src/graphics/framebuffer.sv
@@ -9,9 +9,9 @@ module framebuffer (
   input wire switch_in,
 
   input wire valid_in,
-  input wire [8:0] x,
-  input wire [7:0] y,
-  input wire [7:0] z,
+  input wire [8:0] x_in,
+  input wire [7:0] y_in,
+  input wire [7:0] z_in,
   input wire [11:0] rgb_in,
 
   output logic hsync_out,
@@ -19,9 +19,146 @@ module framebuffer (
   output logic [11:0] rgb_out
 );
 
-  // vga module goes here
+  logic clear;
+  logic clear_buffered;
+  logic [7:0] z;
+  logic [7:0] z_buffered;
+  logic [16:0] write_address;
+  logic [16:0] write_address_buffered;
+  logic [11:0] rgb;
+  logic [11:0] rgb_buffered;
+  logic valid_buffered;
 
-  // two port bram
+  enum {Clear, Write} state;
+
+  logic [8:0] clear_x;
+  logic [7:0] clear_y;
+  
+  logic read_target;
+  logic write_target;
+
+  always_ff @(posedge gpu_clk_in) begin
+    if (rst_in) begin
+      write_target <= 1'b0;
+      read_target <= 1'b1;
+      state <= Clear;
+      clear_x <= 9'd0;
+      clear_y <= 8'd0;
+    end else case (state)
+      Clear: begin
+        if (clear_x == 319) begin
+          clear_x <= 0;
+          if (clear_y == 239) begin
+            clear_y <= 0;
+            state <= Write;
+          end else clear_y <= clear_y + 1;
+        end else clear_x <= clear_x + 1;
+
+        clear <= 1'b1;
+        z <= 8'hFF;
+        write_address <= (clear_y * 9'd320) + clear_x;
+        rgb <= 8'h00;
+      end
+      Write: begin
+        if (switch_in) begin
+          write_target <= !write_target;
+          read_target <= !read_target;
+        end
+
+        if (clear_in) begin
+          state <= Clear;
+        end
+
+        clear <= 1'b0;
+        z <= z_in;
+        write_address <= (y_in * 9'd320) + x_in;
+        rgb <= rgb_in; 
+      end
+    endcase
+  end
+
+  pipe #(
+    .LATENCY(2),
+    .WIDTH(38)
+  ) buffer_pipe (
+    .clk_in(gpu_clk_in),
+    .data_in({clear, z, write_address, rgb}),
+    .data_out({clear_buffered, z_buffered, write_address_buffered, rgb_buffered})
+  );
+
+  valid_pipe #(
+    .LATENCY(3)
+  ) valid_pipe (
+    .clk_in(gpu_clk_in),
+    .rst_in,
+    .valid_in,
+    .valid_out(valid_buffered)
+  );
+
+  logic write_valid;
+  assign write_valid = (valid_buffered && z_buffered < depth) || clear_buffered;
+
+  logic [7:0] depth;
+  depth_ram depth_ram (
+    .clka(gpu_clk_in),
+    .wea(write_valid),
+    .addra(write_address_buffered),
+    .dina(z_buffered),
+    .clkb(gpu_clk_in),
+    .addrb(write_address),
+    .doutb(depth)
+  );
+
+  logic [9:0] hcount;
+  logic [9:0] vcount;
+  logic hsync;
+  logic vsync;
+  logic blank;
+  vga vga (
+    .clk_in(vga_clk_in),
+    .rst_in,
+    .hcount_out(hcount),
+    .vcount_out(vcount),
+    .hsync_out(hsync),
+    .vsync_out(vsync),
+    .blank_out(blank)
+  );
+
+  logic [16:0] read_address;
+  assign read_address = blank ? 17'b0 : ((vcount >> 1) * 9'd320) + (hcount >> 1);
+  logic [1:0][11:0] rgb_outs;
+
+  frame_ram frame0_ram (
+    .clka(gpu_clk_in),
+    .wea(!write_target && write_valid),
+    .addra(write_address_buffered),
+    .dina(rgb_buffered),
+    .clkb(vga_clk_in),
+    .addrb(read_address),
+    .doutb(rgb_outs[0])
+  );
+
+  frame_ram frame1_ram (
+    .clka(gpu_clk_in),
+    .wea(write_target && write_valid),
+    .addra(write_address_buffered),
+    .dina(rgb_buffered),
+    .clkb(vga_clk_in),
+    .addrb(read_address),
+    .doutb(rgb_outs[1])
+  );
+
+  logic blank_out;
+  pipe #(
+    .LATENCY(2),
+    .WIDTH(3)
+  ) sync_pipe (
+    .clk_in(vga_clk_in),
+    .data_in({hsync, vsync, blank}),
+    .data_out({hsync_out, vsync_out, blank_out})
+  );
+
+  assign rgb_out = blank_out ? 12'b0 : (read_target ? rgb_outs[1] : rgb_outs[0]);
 
 endmodule
 
