5dc47fc46b38ae97fc865670d7e7a73d57014418
Kailas B. Kahler <kailasbk230@gmail.com>
2022-11-22 23:26:49 -0500

    Add unit cube mesh with random colors

--- data/gen_vertices.py
+++ data/gen_vertices.py
@@ -1,4 +1,5 @@
 import struct
+import random
 
 def hex_from_float(f):
     return hex(struct.unpack('I', struct.pack('f', f))[0]).upper()[2:].zfill(8)
@@ -9,22 +10,60 @@ def hex_from_tuple(t):
 
 # vertices to convert to mem file
 vertices = [
-    (0.0, 0.666666, 0.5),
-    (-0.375, 0.3333333, 0.5),
-    (0.25, 0.1666666, 0.5),
-    (0.4, -0.1, 0.5),
-    (-0.6, 0.6, 0.2),
-    (-0.3, 0.1, 0.2),
-    (0.1, 0.0, 0.2),
+    (0.499969, -0.499969, 0.499969),
+    (-0.499969, -0.499969, -0.499969),
+    (-0.499969, -0.499969, 0.499969),
+    (0.499969, 0.499969, -0.499969),
+    (-0.499969, 0.499969, -0.499969),
+    (0.499969, -0.499969, -0.499969),
+    (0.499969, 0.499969, 0.499969),
+    (-0.499969, 0.499969, 0.499969),
+]
+
+colors = [(random.randint(6, 15), random.randint(6, 15), random.randint(6, 15)) for v in vertices]
+
+triangles = [
+    (0, 1, 2),
+    (1, 3, 4),
+    (5, 6, 3),
+    (7, 3, 6),
+    (2, 4, 7),
+    (0, 7, 6),
+    (0, 5, 1),
+    (2, 1, 4),
+    (0, 2, 7),
+    (1, 5, 3),
+    (5, 0, 6),
+    (7, 4, 3),
 ]
 
 contents = ''
 for vertex in vertices:
     contents += hex_from_tuple(vertex) + '\n'
 
-contents += '00000000_00000000_00000000\n'
+contents += 'FFFFFFFF_FFFFFFFF_FFFFFFFF\n'
 
 f = open('./data/vertices.mem', 'w')
 f.write(contents)
 f.close()
 
+contents = ''
+for color in colors:
+    contents += ''.join([hex(comp)[2:].upper().zfill(1) for comp in color]) + '\n'
+
+contents += 'FFF\n'
+
+f = open('./data/colors.mem', 'w')
+f.write(contents)
+f.close()
+
+contents = ''
+for triangle in triangles:
+    for index in triangle:
+        contents += hex(index)[2:].upper().zfill(3) + '\n'
+
+contents += 'FFF\n'
+
+f = open('./data/indices.mem', 'w')
+f.write(contents)
+f.close()
