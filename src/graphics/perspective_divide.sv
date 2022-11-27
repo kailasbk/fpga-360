2e5d7e6e21809f0f77b13195eb734b8408bb56bd
Kailas B. Kahler <kailasbk230@gmail.com>
2022-11-27 00:10:35 -0500

    Implement perspective divide

--- src/graphics/perspective_divide.sv
+++ src/graphics/perspective_divide.sv
@@ -12,19 +12,51 @@ module perspective_divide (
   output logic [3:0][31:0] vertex_out
 );
 
-  valid_pipe #(
-    .LATENCY(1)
-  ) valid_pipe (
+  logic x_valid;
+  fp32_div x_div (
     .clk_in,
     .rst_in,
     .valid_in,
-    .valid_out
+    .a_in(vertex_in[0]),
+    .b_in(vertex_in[3]),
+    .valid_out(x_valid),
+    .c_out(vertex_out[0])
   );
 
-  // placeholder
-  always_ff @(posedge clk_in) begin
-    vertex_out <= vertex_in;
-  end
+  logic y_valid;
+  fp32_div y_div (
+    .clk_in,
+    .rst_in,
+    .valid_in,
+    .a_in(vertex_in[1]),
+    .b_in(vertex_in[3]),
+    .valid_out(y_valid),
+    .c_out(vertex_out[1])
+  );
+
+  logic z_valid;
+  fp32_div z_div (
+    .clk_in,
+    .rst_in,
+    .valid_in,
+    .a_in(vertex_in[2]),
+    .b_in(vertex_in[3]),
+    .valid_out(z_valid),
+    .c_out(vertex_out[2])
+  );
+
+  logic w_valid;
+  fp32_div w_div (
+    .clk_in,
+    .rst_in,
+    .valid_in,
+    .a_in(32'h3F800000),
+    .b_in(vertex_in[3]),
+    .valid_out(w_valid),
+    .c_out(vertex_out[3])
+  );
+
+  assign valid_out = x_valid && y_valid && z_valid && w_valid;
 
 endmodule
 
